f24e78af38d7df531d3081cbfbfb7036d5c5c429
mitzukodavis <mitzukodavis@gmail.com>
2024-02-19 23:58:33 +0000

    er

--- lib/main.dart
+++ lib/main.dart
@@ -10,11 +10,13 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         body:  Center(
-          child: Text("Hello idx")
+          child: 
+          Text("Hello idx",style: TextStyle(fontWeight: FontWeight.bold, fontSize: 100
+          ),)
         )
       )
     );
